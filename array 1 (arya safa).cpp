6b310009fcdae4332c64847d6021822cca1c847c
adilaryasafa <97731247+adilaryasafa@users.noreply.github.com>
2022-02-25 15:38:51 +0700

    Add files via upload

---
+++ array 1 (arya safa).cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	int nilai[3] = { 8, 9, 7};
+	string nama[3];
+	nama[0] = "NAMA : MOH. ADIL ARYA SAFA\n";
+	nama[1] = "NIM  : 07.1.2021.0014\n";
+	nama[2] = "Prodi: TEKNIK INFORMATIKA\n";
+	cout<<nama[0]<<nama[1]<<nama[2]<<endl;
+	cout<<nilai[3];
+}
